10b3bc15842f837fb1417fe1764b600ef090b84f
Ilse Macías <ilse.macias@gmail.com>
2018-01-10 14:56:41 -0800

    Automation testing, selenium w/NUnit.

--- SeleniumNUnit/Program.cs
+++ SeleniumNUnit/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +9,46 @@ using System.Threading.Tasks;
 
 namespace SeleniumNUnit
 {
-    class Program
+    public class Program
     {
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
+          
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
+            Console.WriteLine("Open the URL");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //EnterText(element, value, type)
+
+            //Title
+            SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+
+            //Initial
+            SeleniumSetMethods.EnterText(driver, "Initial", "Mr. Shofo", "Name");
+
+
+            Console.WriteLine("The value form my title is: " + SeleniumGetMethods.GetText(driver, "TitleId", "Id"));
+            Console.WriteLine("The value form my Initial is: " + SeleniumGetMethods.GetText(driver, "Initial", "Name"));
+
+            //Click
+            SeleniumSetMethods.Click(driver, "Save", "Name");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
         }
     }
+
 }
---
+++ SeleniumNUnit/SeleniumGetMethods.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumNUnit
+{
+    public class SeleniumGetMethods
+    {
+        public static string GetText(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                return driver.FindElement(By.Id(element)).GetAttribute("value");
+
+            if (elementType == "Name")
+                return driver.FindElement(By.Name(elementType)).GetAttribute("value");
+
+            else return String.Empty;
+        }
+
+        public static string GetTextDropDownList(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                return new SelectElement(driver.FindElement(By.Id(element))).AllSelectedOptions.SingleOrDefault().Text;
+             
+
+            if (elementType == "Name")
+                return new SelectElement(driver.FindElement(By.Name(element))).AllSelectedOptions.SingleOrDefault().Text;
+
+            else return String.Empty;
+        }
+    }
+}
